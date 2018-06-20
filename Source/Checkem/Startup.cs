5ada60be959c5440120823480e0b0d6f302fff81
Steffen Cole Blake <steffen.cole.blake@gmail.com>
2018-06-19 19:53:53 -0600

    [1.0.2.0] Wiki format option

--- Source/Checkem/Startup.cs
+++ Source/Checkem/Startup.cs
@@ -3,13 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using EliteMMO.API;
 
-
 namespace Checkem
 {
     public class Startup
     {
         public static void Main(params string[] args)
         {
+            var format = "normal";
+            if (args.Any())
+                for (var n = 0; n < args.Length; n += 2)
+                {
+                    var cmd = args[n].ToLower().Substring(0, 2);
+                    switch (cmd)
+                    {
+                        case "-f":
+                            format = args[n + 1].ToLower();
+                            break;
+                        default:
+                            Console.WriteLine("Unrecognized arg '{0}'", args[n]);
+                            Close();
+                            break;
+                    }
+                }
+
             var instanceFactory = new InstanceFactory();
 
             var names = instanceFactory.GetCharacterNames().ToArray();
@@ -26,21 +42,38 @@ namespace Checkem
 
             // Get all of our trusts
             var trustList = GetTrusts(instance)
-                .OrderBy(t => t.Icon)
+                .OrderBy(t => t.Type)
                 .ThenBy(t => t.Name)
                 .ToArray();
 
+            if (format == "wiki")
+                Console.WriteLine("{{Trust Checklist \n| Complete - Color = green \n| Default - Color = black");
+
             // Output our data
-            foreach (var (HasSpell, Name, _) in trustList)
+            foreach (var trustGroup in trustList.GroupBy(t => t.Type))
             {
-                Console.WriteLine("[{0}] {1}", HasSpell ? "√" : " ", Name);
+                if (format == "normal")
+                    Console.WriteLine("==={0}===", trustGroup.Key);
+
+                foreach (var trust in trustGroup)
+                    if (format == "normal")
+                        Console.WriteLine("[{0}] {1}", trust.HasSpell ? "√" : " ", trust.Name);
+                    else if (format == "wiki")
+                        Console.WriteLine("| {0} = {1}", trust.Name, trust.HasSpell ? "Complete" : "");
             }
 
-            // Aggregate data as well
-            var hasTotal = trustList.Count(t => t.HasSpell);
-            var total = trustList.Length;
+            if (format == "normal")
+            {
+                // Aggregate data as well
+                var hasTotal = trustList.Count(t => t.HasSpell);
+                var total = trustList.Length;
 
-            Console.WriteLine("Total: {0}/{1}", hasTotal, total);
+                Console.WriteLine("Total: {0}/{1}", hasTotal, total);
+            }
+            else if (format == "wiki")
+            {
+                Console.WriteLine("}}");
+            }
 
             Close();
         }
@@ -91,7 +124,7 @@ namespace Checkem
             Environment.Exit(-1);
         }
 
-        private static IEnumerable<(bool HasSpell, string Name, ushort Icon)> GetTrusts(EliteAPI instance)
+        private static IEnumerable<(bool HasSpell, string Name, string Type)> GetTrusts(EliteAPI instance)
         {
             // Iterate from 0 to 10,000 because I dunno how to get every spell that exists. It seems to be fast this way anyways
             for (uint n = 0; n < 10000; n++)
@@ -117,8 +150,24 @@ namespace Checkem
                 var hasTrust = instance.Player.HasSpell(spell.Index);
                 var icon = spell.ListIcon2;
 
+                var type =
+                    icon <= 1009
+                        ? "Tank"
+                        : icon <= 1018
+                            ? "Melee Fighter"
+                            : icon <= 1027
+                                ? "Ranged Fighter"
+                                : icon <= 1036
+                                    ? "Offensive Caster"
+                                    : icon <= 1045
+                                        ? "Healer"
+                                        : icon <= 1054
+                                            ? "Support"
+                                            : "Other";
+
+
                 // Ok yeah it is a real trust
-                yield return (hasTrust, name, icon);
+                yield return (hasTrust, name, type);
             }
         }
     }
